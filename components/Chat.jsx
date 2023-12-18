8cf82e22a0d07d90bc0697b19d892bf2a28bd33d
Robbie213 <sasnarinedeodat407@gmail.com>
2023-12-18 16:54:34 -0500

    React Query - Setup

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,14 +1,20 @@
 "use client";
 
 import { useState } from "react";
+import { generateChatResponse } from "@/utils/action";
+import { useMutation } from "@tanstack/react-query";
 
 export default function Chat() {
   const [text, setText] = useState("");
   const [message, setMessage] = useState([]);
 
+  const { mutate } = useMutation({
+    mutationFn: (message) => generateChatResponse(message),
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(text);
+    mutate(text);
   };
   return (
     <div className="min-h-[calc(100vh-6rem)] grid grid-rows-[1fr,auto]">
