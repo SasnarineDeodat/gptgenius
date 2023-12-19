b4c95d69b87dae1ddefb2411a51f0e6ab9c79eee
Robbie213 <sasnarinedeodat407@gmail.com>
2023-12-19 17:19:30 -0500

    GetAllTours Function

--- utils/actions.js
+++ utils/actions.js
@@ -74,9 +74,37 @@ export const getExistingTour = async ({ city, country }) => {
     },
   });
 };
-
 export const createNewTour = async (tour) => {
   return prisma.tour.create({
     data: tour,
   });
+};
+
+export const getAllTours = async (searchTerm) => {
+  if (!searchTerm) {
+    const tours = await prisma.tour.findMany({
+      orderBy: {
+        city: "asc",
+      },
+    });
+    return tours;
+  }
+  const tours = await prisma.tour.findMany({
+    where: {
+      OR: [
+        {
+          city: {
+            contains: searchTerm,
+          },
+          country: {
+            contains: searchTerm,
+          },
+        },
+      ],
+    },
+    orderBy: {
+      city: "asc",
+    },
+  });
+  return tours;
 };
