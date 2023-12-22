b311e8ddf1edd6b3eedde5567d8eb79ad81ce002
Robbie213 <sasnarinedeodat407@gmail.com>
2023-12-22 14:43:41 -0500

    Tours Page - Setup

--- app/(dashboard)/tours/page.js
+++ app/(dashboard)/tours/page.js
@@ -1,3 +1,21 @@
-export default function page() {
-  return <div>ToursPage</div>;
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import ToursPage from "@/components/ToursPage";
+import { getAllTours } from "@/utils/actions";
+
+export default async function AllToursPage() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["tasks"],
+    queryFn: () => getAllTours(),
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ToursPage />
+    </HydrationBoundary>
+  );
 }
---
+++ components/ToursList.jsx
@@ -0,0 +1,3 @@
+export default function ToursList() {
+  return <div>ToursList</div>;
+}
